047df925ebaab1cde496ab543e60521aa6e757e3
jbeemster <joshua.a.beemster@gmail.com>
2025-06-17 13:07:29 +0200

    Prepared for release

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 locals {
   module_name    = "bigquery-loader-pubsub-ce"
-  module_version = "0.3.2"
+  module_version = "0.4.0"
 
   app_name    = "snowplow-bigquery-loader"
   app_version = var.app_version
